70265d9127815f81ae8450dfa9d32e15397f4bd0
Hippolyte L. DEBERNARDI <contact@hippolyte-debernardi.com>
2020-07-18 01:06:02 +0200

    fixed dockerfile

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,6 @@
 import time
 from flask import Flask
 
-
 app = Flask(__name__)
 
 
@@ -10,10 +9,6 @@ def n():
     return "Le N. a parlé"
 
 
-@app.route('/api/time')
-def get_current_time():
+@app.route('/')
+def hello():
     return {'time': time.time()}
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, port=5050) # threaded=True
